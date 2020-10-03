b0aeae115aaa824d5d5b25bf4462a19a1d82f7cc
Agung <Source>
2020-10-03 12:44:45 +0700

    Rest API Authentication By Agung Panduan .COM

---
+++ RestAPIAuthenticationToken/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestAPIAuthenticationToken.WebApiApplication" Language="C#" %>
---
+++ RestAPIAuthenticationToken/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using Microsoft.Owin.Security.OAuth;
+using System.Net.Http;
+using RestAPIAuthenticationToken;
+
+//[assembly: OwinStartup(typeof(RestAPIAuthenticationToken.Startup))]
+
+namespace RestAPIAuthenticationToken
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            //enable cors origin requests
+            //agungpanduan.com
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+
+            var myProvider = new MyAuthorizationServerProvider();
+            OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        
+            System.Web.Http.HttpConfiguration config = new System.Web.Http.HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
